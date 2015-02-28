e2be409a5e18f7d6af971dd5cb902e4e1c6ab3d9
Luigi Garcia <mr.garcialuigi@gmail.com>
2015-02-27 21:47:21 -0300

    Problem1 Solution

---
+++ ProjectEulerFSharp/ProjectEulerFSharp/Problem1.fsx
@@ -0,0 +1,15 @@
+﻿let targetNum = 999
+let range = [1..targetNum]
+
+let testAndSum (predicate) (x:list<int>) = 
+    x |> List.filter (predicate) 
+      |> List.sum 
+
+// solution 1
+let result = range |> List.filter (fun x -> x % 3 = 0 || x % 5 = 0) 
+                   |> List.sum
+
+// solution 2
+range 
+|> testAndSum (fun x -> x % 3 = 0 || x % 5 = 0)
+|> printfn "The sum is %d"
