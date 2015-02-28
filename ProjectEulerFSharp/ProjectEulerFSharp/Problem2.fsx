2ac19dea9aa858c4292f5d61364b9768661c8a69
Luigi Garcia <mr.garcialuigi@gmail.com>
2015-02-27 21:14:24 -0800

    Problem2 Solution

---
+++ ProjectEulerFSharp/ProjectEulerFSharp/Problem2.fsx
@@ -0,0 +1,23 @@
+﻿open System.Collections.Generic
+
+let printList (l : List<_>) =
+    printfn "l.Count: %i, l.Capacity: %i" l.Count l.Capacity
+    printfn "Items:"
+    l |> Seq.iteri (fun index item ->
+        printfn "    l.[%i]: %d" index l.[index])
+    printfn "-----------"
+
+let myList = new List<int>()
+myList.Add(0)
+myList.Add(1)
+
+while myList.[myList.Count - 1] < 4000000 do
+   myList.Add(myList.[myList.Count - 1] + myList.[myList.Count - 2])
+
+myList.RemoveAt(myList.Count - 1)
+
+printList myList
+
+myList 
+|> Seq.filter (fun x -> x % 2 = 0)
+|> Seq.sum
